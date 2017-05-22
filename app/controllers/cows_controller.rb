9f3d401021dbefeaca264c02491e79f923038653
claire schults <claire.schults@gmail.com>
2017-05-22 17:03:33 +0200

    Cow / User / Booking Models & Cow Controller

--- app/controllers/cows_controller.rb
+++ app/controllers/cows_controller.rb
@@ -1,2 +1,34 @@
 class CowsController < ApplicationController
+  before_action :set_cow, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cows = Cow.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @cow = Cow.new
+  end
+
+  def create
+    @cow = Cow.new(cows_params)
+    if @cow.save
+      redirect_to cow_path(@cow)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_cow
+    @cow = Cow.find(params[:id])
+  end
+
+  def cows_params
+    params.require(:cows).permit(:name, :race, :description, :address)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cow.create(name: "Marguerite", address: "Périgord", description: "La Prim’Holstein est la race laitière la plus répandue en France et dans le monde. Sa robe pie-noire, parfois pie-rouge, a d’ailleurs largement marqué les esprits. Elle se distingue également par sa grande taille et ses records de production laitière.", race: "Prim’Holstein", user: User.first)
+Cow.create(name: "Melusine", address: "Périgord", description: "La Prim’Holstein est la race laitière la plus répandue en France et dans le monde. Sa robe pie-noire, parfois pie-rouge, a d’ailleurs largement marqué les esprits. Elle se distingue également par sa grande taille et ses records de production laitière.", race: "Prim’Holstein", user: User.first)
+Cow.create(name: "Merise", address: "Périgord", description: "La Prim’Holstein est la race laitière la plus répandue en France et dans le monde. Sa robe pie-noire, parfois pie-rouge, a d’ailleurs largement marqué les esprits. Elle se distingue également par sa grande taille et ses records de production laitière.", race: "Prim’Holstein", user: User.first)
+Cow.create(name: "Marguarita", address: "Périgord", description: "La Prim’Holstein est la race laitière la plus répandue en France et dans le monde. Sa robe pie-noire, parfois pie-rouge, a d’ailleurs largement marqué les esprits. Elle se distingue également par sa grande taille et ses records de production laitière.", race: "Prim’Holstein", user: User.first)
+Cow.create(name: "Marie", address: "Périgord", description: "La Prim’Holstein est la race laitière la plus répandue en France et dans le monde. Sa robe pie-noire, parfois pie-rouge, a d’ailleurs largement marqué les esprits. Elle se distingue également par sa grande taille et ses records de production laitière.", race: "Prim’Holstein", user: User.first)
